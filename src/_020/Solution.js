55354461876c56c693013efe6cd7d82515dca6e0
钱毓婷 <taryn2016@outlook.com>
2018-04-18 23:57:10 +0800

    fix: Delete excess code

--- src/_020/Solution.js
+++ src/_020/Solution.js
@@ -3,36 +3,34 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    if (s === '') {
-        return true
-    }
-    if (s.length % 2 !== 0) {
-        return false
-    } else {
-		var strArr1 = []
-        var num = 0
-        for (var i = 0; i < s.length; i++) {
-            if (isLeft(s[i])) {
-                strArr1.push(s[i])
-            } else {
-				var pop = strArr1.pop()
-                if (!squre(pop, s[i])) {
-					console.log(pop)
-                    return false
-                }
-            }
-        }
-        return (strArr1.length === 0)
-
-    }
-    function isLeft(a) {
-        return (a==='{'||a==='['||a==='(')
-    }
-    function squre(a,b) {
-        if ((a ==='[' && b ===']') || (a ==='{' && b ==='}') || (a ==='(' && b ===')')) {
-            return true
-        } else {
+  if (s === '') {
+    return true
+  }
+  if (s.length % 2 !== 0) {
+    return false
+  } else {
+	var strArr1 = []
+    var num = 0
+    for (var i = 0; i < s.length; i++) {
+      if (isLeft(s[i])) {
+        strArr1.push(s[i])
+      } else {
+		      var pop = strArr1.pop()
+          if (!squre(pop, s[i])) {
             return false
+          }
         }
     }
+    return (strArr1.length === 0)
+  }
+  function isLeft(a) {
+    return (a==='{'||a==='['||a==='(')
+  }
+  function squre(a,b) {
+    if ((a ==='[' && b ===']') || (a ==='{' && b ==='}') || (a ==='(' && b ===')')) {
+      return true
+    } else {
+      return false
+    }
+  }
 };
