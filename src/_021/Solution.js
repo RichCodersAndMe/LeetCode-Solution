cfffaa355d74505d2e5e63b7a94d5f754e35d4cd
钱毓婷 <taryn2016@outlook.com>
2018-04-23 22:54:02 +0800

    feat: add solution of `Merge Two Sorted Lists`(021) with javascript

---
+++ src/_021/Solution.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    var mergedHead = { val : -1, next : null },
+        crt = mergedHead;
+    while(l1 && l2) {
+        if(l1.val > l2.val) {
+            crt.next = l2;
+            l2 = l2.next;
+        } else {
+            crt.next = l1;
+            l1 = l1.next;
+        }
+        crt = crt.next;
+    }
+    crt.next = l1 || l2;
+
+    return mergedHead.next;
+};
